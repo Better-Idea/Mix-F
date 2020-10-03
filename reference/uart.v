da67e1de7e14216584684ff0d15210566673be87
PowerfulCat <x4495@outlook.com>
2020-10-03 11:44:39 +0800

    更新 reference/uart.v

--- reference/uart.v
+++ reference/uart.v
@@ -1,88 +1,76 @@
-`timescale 1ns / 1ps
 
 module uart(
-    input                   reset,
-    input                   clock_50mhz,
-    input                   rx,
-    output                  tx
+    input                   i_extern_reset,
+    input                   i_clock_50mhz,
+    input                   i_rx,
+    output                  o_tx
 );
+    wire                    i_reset;
+    wire                    i_clock_x8;
+    wire [ 7:0]             data_rx;
+    reg  [ 7:0]             buf_rx [0:3];
+    reg  [ 7:0]             buf_tx              = 0;
+    reg  [ 1:0]             idx_rx              = 0;
+    reg  [ 1:0]             idx_tx              = 0;
+    reg                     i_load_ack          = 0;
+    wire                    o_error_parity;
+    wire                    o_error_stop_bit;
+    wire                    o_store_req;
+    wire                    i_clock_rx          = i_clock_50mhz;
 
-    reg  [ 7:0] counter_1mhz            = 0;
-    reg  [ 2:0] counter_125khz          = 0;
+    wire                    i_clock_tx;
+    wire                    o_load_req;
 
-    reg         clock_1mhz              = 0;
-    reg         clock_125khz            = 0;
-    reg         ack_load                = 0;
-    wire        req_load;
-    reg  [15:0] obits                   = 0;
-    reg  [ 7:0] obuf [0:3];
-    reg  [ 1:0] i_rx                    = 0;
-    reg  [ 1:0] i_tx                    = 0;
-
-    wire        error_parity;
-    wire        error_stop_bit;
-    wire        req_store;
-    wire [15:0] bits;
-
-    uart_tx u0(
-        .reset(reset),
-        .clock(clock_125khz),
-        .parity(0),
-        .width(8),
-        .bits(obits),
-        .ack_load(ack_load),
-        .req_load(req_load),
-        .out(tx)
+    uart_rx u_rx(
+        .i_reset(i_reset),
+        .i_clock_x8(i_clock_rx),
+        .i_parity(0),
+        .i_data_width(8),
+        .i_data(i_rx),
+        .o_error_parity(o_error_parity),
+        .o_error_stop_bit(o_error_stop_bit),
+        .o_store_req(o_store_req),
+        .o_data(data_rx)
     );
 
-    uart_rx u1(
-        .reset(reset),
-        .clock_x8(clock_1mhz),
-        .parity(0),
-        .width(8),
-        .in(rx),
-        .error_parity(error_parity),
-        .error_stop_bit(error_stop_bit),
-        .req_store(req_store),
-        .bits(bits)
+    reset u_reset(
+        .i_extern_reset(i_extern_reset),
+        .i_clock_xxmhz(i_clock_50mhz),
+        .o_reset(i_reset)
     );
 
-always @ (negedge reset or posedge clock_50mhz) begin
-    if (reset == 0) begin
-        counter_1mhz                        = 0;
-        clock_1mhz                          = 0;
-    end else begin
-        // 50mhz / 50 -> 1mhz
-        clock_1mhz                          = counter_1mhz == 49;
-        counter_1mhz                        = clock_1mhz ? 0 : counter_1mhz + 1; 
+    clock_division #(
+        .max_counter(8)
+    )div_at_6p25m(
+        .i_reset(i_reset),
+        .i_clock(i_clock_50mhz),
+        .i_top(8),
+        .o_clock(i_clock_tx)
+    );
+
+    uart_tx u_tx(
+        .i_reset(i_reset),
+        .i_clock(i_clock_tx),
+        .i_parity(0),
+        .i_data_width(8),
+        .i_data(buf_tx),
+        .i_load_ack(i_load_ack),
+        .o_load_req(o_load_req),
+        .o_data(o_tx)
+    );
+
+    wire    need_load           = i_load_ack != o_load_req;
+
+always @ (posedge i_clock_rx) begin
+    if (o_store_req) begin
+        buf_rx[idx_rx]          = data_rx;
+        idx_rx                  = idx_rx + 1;
     end
-end
 
-always @ (negedge reset or posedge clock_1mhz) begin
-    if (reset == 0) begin
-        counter_125khz                      = 0;
-        clock_125khz                        = 0;
-    end else begin
-        // 1mhz / 8 -> 125khz
-        clock_125khz                        = counter_125khz == 7;
-        counter_125khz                      = clock_125khz ? 0 : counter_125khz + 1;
-    end
-end
-
-always @ (posedge req_store) begin
-    obuf[i_rx]                              = bits;
-    i_rx                                    = i_rx + 1;
-end
-
-always @ (negedge reset or negedge clock_125khz) begin
-    if (reset == 0) begin
-        ack_load                            = 0;
-    end else if (i_tx == i_rx) begin
-        ack_load                            = 0;
-    end else if (req_load) begin
-        obits                               = obuf[i_tx];
-        i_tx                                = i_tx + 1;
-        ack_load                            = 1;
+    if (need_load && idx_tx != idx_rx) begin
+        buf_tx                  = buf_rx[idx_tx];
+        idx_tx                  = idx_tx + 1;
+        i_load_ack              = o_load_req;
     end
 end
 
