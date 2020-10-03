f637ecd7daba9ef0ae06faaa7ec0cdda9a3c1dbe
PowerfulCat <x4495@outlook.com>
2020-10-03 11:43:01 +0800

    重命名 interface/define.v

--- interface/define.v
+++
@@ -1,8 +0,0 @@
-`ifndef xinterface_define
-`define xinterface_define
-
-`define uart_parity_none        0
-`define uart_parity_even        2
-`define uart_parity_odd         3
-
-`endif
---
+++ interface/uart_define.v
@@ -0,0 +1,8 @@
+`ifndef xinterface_uart_define
+`define xinterface_uart_define
+
+`define uart_parity_none        0
+`define uart_parity_even        2
+`define uart_parity_odd         3
+
+`endif
