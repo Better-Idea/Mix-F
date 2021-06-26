c15d570fba0c3a20153cb4dfebe3ef2ba238d5b2
PowerfulCat <x4495@outlook.com>
2021-06-26 22:23:22 +0800

    完善原有功能 @ extern/arm/vcpu.v

--- extern/arm/vcpu.v
+++ extern/arm/vcpu.v
@@ -1,55 +1,74 @@
 // 目标：低功耗设计
 `timescale 1ns/1ps
 
-`define PC              15
-`define INT_MSB         31
+`define PC              4'd15
+`define REG_MSB         31
 `define ALL_ONE         32'hffff_ffff
 `define ALL_ZERO        32'h0000_0000
+`define NOT_IN_ITB      (!in_it_block)
+`define IN_ITB          (in_it_block)
+`define SYS_BITS        32
 
 module vcpu(
     input           sck,
     input [15:0]    cmd
 );
+    // public
     reg [31:0] r[0:15];
-    reg [31:0] a;   // adddition
-    reg [31:0] b;   // temp
     reg in_it_block = 0;
     reg cf = 0;
     reg zf = 0;
     reg nf = 0;
     reg vf = 0;
-    
+
+    // private
+    reg [31:0] a;   // adddition
+    reg [31:0] b;   // temp
     reg tmp_cf = 0;
     reg tmp_zf = 0;
     reg tmp_nf = 0;
     reg tmp_vf = 0;
-    reg sub_cf = 0;
-    reg need_modify_vf = 0;
+    reg msb_eq = 0;
 
+    // 修改该标志位寄存器
+    reg modify_state = 0;
     reg m0;
     reg m1;
 always @(posedge sck) begin
     casez(cmd[15:8])
         // 移位操作
         8'b000?_????: begin
-            `define MODE        cmd[12:11]
-            `define WITH_SIGN   cmd[12]
-            `define IMM5        cmd[10:6]
-            `define RM          cmd[5:3]
-            `define RD          cmd[2:0]
-            
-            if (`MODE == 0) begin
-                { tmp_cf, r[`RD] } = { r[`RM] } << `IMM5;
-            end else begin
-                { r[`RD], tmp_cf } = { 
-                    `WITH_SIGN && r[`RM][`INT_MSB] ? `ALL_ONE : `ALL_ZERO, r[`RM], 1'b0
-                } >> `IMM5;
-            end
+            `define MODE        (cmd[12:11])
+            `define WITH_SIGN   (cmd[12])
+            `define IMM5        (cmd[10:6])
+            `define RM          (cmd[5:3])
+            `define RD          (cmd[2:0])
+            `define LEFT        (`IMM5)
+            `define RIGHT       (`IMM5 == 0 ? 32 : `IMM5)
 
-            if (`MODE != 0 && !in_it_block) begin
-                nf = r[`RD][`INT_MSB];
-                zf = r[`RD] == 0;
-                cf = tmp_cf;
+            // 逻辑右移、算数右移
+            // T1:LSR RD, RM
+            // T1:ASR RD, RM
+            if (`MODE != 0) begin
+                { r[`RD], tmp_cf } = {
+                    `WITH_SIGN && r[`RM][`REG_MSB] ? `ALL_ONE : `ALL_ZERO, r[`RM], 1'b0
+                } >> `RIGHT;
+                { b } = r[`RD];
+                { modify_state } = `NOT_IN_ITB;
+                { tmp_vf } = vf;
+            // T2：mov rm, rd 不允许在 IT 块中
+            end else if (`IMM5 == 0 && `IN_ITB) begin
+                // error
+                // TODO:=============================
+            // 逻辑左移、赋值
+            // 当赋值的目的寄存器是 PC 寄存器时不改变状态位
+            // T2: MOV RM, RD
+            // T1: LSL RM, RD, IMM5
+            end else begin
+                { tmp_cf, r[`RD] } = { cf, r[`RM] } << `LEFT;
+                { b } = r[`RD];
+                { modify_state } = { `IMM5, `RD } != { 5'b0, `PC } && `NOT_IN_ITB;
+                { tmp_vf } = vf;
             end
 
             // TODO: opcode 2'b11 invalid
@@ -58,34 +77,34 @@ always @(posedge sck) begin
             `undef IMM5
             `undef RM
             `undef RD
+            `undef LEFT
+            `undef RIGHT
         end
 
         // 加减法
-        8'b0001_????: begin
-            `define IS_IMM_RM   cmd[10]
+        8'b0001_1???: begin
+            `define IS_IMM_RM   (cmd[10])
 
             // cmd[9]
             // - 0:add
             // - 1:sub
-            `define IS_SUB      cmd[9]
+            `define IS_SUB      (cmd[9])
 
-            `define RM          cmd[8:6]
-            `define RN          cmd[5:3]
-            `define RD          cmd[2:0]
+            `define RM          (cmd[8:6])
+            `define RN          (cmd[5:3])
+            `define RD          (cmd[2:0])
 
+            // T1: ADD RD, RN, RM
+            // T1: SUB RD, RN, RM
+            // T1: ADD RD, RN, IMM3
+            // T1: SUB RD, RN, IMM3
             { a } = `IS_IMM_RM ? `RM : r[`RM];
-            { b, sub_cf } = `IS_SUB ? { ~a, 1'b1 } : { a, 1'b0 };
-            { tmp_vf } = r[`RN][`INT_MSB] == b[`INT_MSB];
-            { tmp_cf, r[`RD] } = r[`RN] + b + sub_cf;
-
-            if (`RD != `PC && !in_it_block) begin
-                nf = r[`RD][`INT_MSB];
-                zf = r[`RD] == 0;
-                cf = tmp_cf;
-
-                // 操作前相同符号位，相加后符号位不同了就表示溢出了
-                vf = tmp_vf ? tmp_vf != r[`RD][`INT_MSB] : 0;
-            end
+            { b } = `IS_SUB ? -a : a;
+            { msb_eq } = r[`RN][`REG_MSB] == b[`REG_MSB];
+            { tmp_cf, r[`RD] } = r[`RN] + b;
+            { b } = r[`RD];
+            { modify_state } = `RD != `PC && `NOT_IN_ITB;
+            { tmp_vf } = msb_eq ? r[`RN][`REG_MSB] != b[`REG_MSB] : 0;
 
             `undef IS_IMM_RM
             `undef IS_SUB
@@ -96,42 +115,34 @@ always @(posedge sck) begin
 
         // 与立即数加、减、赋值、比较
         8'b001?_????: begin
-            `define MODE        cmd[12:11]
             `define IS_MOV      (`MODE == 0)
-            `define IS_NOT_MOV  (`MODE != 0)
             `define IS_NOT_CMP  (`MODE != 1)
-            `define IS_SUB      cmd[11]
-            `define RD          cmd[10:8]
-            `define IMM8        cmd[7:0]
+            `define IS_SUB      (cmd[11])
+            `define MODE        (cmd[12:11])
+            `define RDN         (cmd[10:8])
+            `define IMM8        (cmd[7:0])
 
-            { b, sub_cf } = `IS_SUB ? { ~`IMM8, 1'b1 }: { `IMM8, 1'b0 };
-            { a } = `IS_MOV ? 0 : r[`RD];
-            { tmp_vf } = r[`RD][`INT_MSB] == b[`INT_MSB];
-            { tmp_cf, b } = a + b + sub_cf;
-
-            // 不限制寄存器
-            if (!in_it_block) begin
-                nf = r[`RD][`INT_MSB];
-                zf = r[`RD] == 0;
-                cf = tmp_cf;
-
-                // mov 不修改 vf
-                if (`IS_NOT_MOV) begin
-                    vf = tmp_vf ? tmp_vf != r[`RD][`INT_MSB] : 0;
-                end
-            end
+            // T1: MOV RD, IMM8
+            // T1: CMP RN, IMM8
+            // T2: ADD RDN, IMM8
+            // T2: SUB RDN, IMM8
+            { a } = `IS_MOV ? `ALL_ZERO : r[`RDN];
+            { b } = `IS_SUB ? -`IMM8 : `IMM8;
+            { msb_eq } = r[`RDN][`REG_MSB] == b[`REG_MSB];
+            { tmp_cf, b } = a + b;
+            { modify_state } = `NOT_IN_ITB;
+            { tmp_vf } = `IS_MOV ? vf : (msb_eq ? r[`RDN][`REG_MSB] != b[`REG_MSB] : 0);
 
             // cmp 不修改目的寄存器
             if (`IS_NOT_CMP) begin
-                r[`RD] = b;
+                r[`RDN] = b;
             end
 
-            `undef MODE
             `undef IS_MOV
-            `undef IS_NOT_MOV
             `undef IS_NOT_CMP
             `undef IS_SUB
-            `undef RD
+            `undef MODE
+            `undef RDN
             `undef IMM8
         end
 
@@ -153,10 +164,13 @@ always @(posedge sck) begin
             `define IS_BIC      (cmd[9:6] == 4'b1110)
             `define IS_MVN      (cmd[9:6] == 4'b1111)
             `define IS_NOT_TST  (!`IS_TST)
+            `define IS_NOT_CMP  (!`IS_CMP)
+            `define IS_NOT_CMN  (!`IS_CMN)
+            `define IS_NOT_MVN  (!`IS_MVN)
             `define RM          (cmd[5:3])
             `define RDN         (cmd[2:0])
-            `define LEFT        (r[`RM][4:0])
-            `define RIGHT       (r[`RM][4:0] == 0 ? 32 : r[`RM][4:0])
+            `define LEFT        (r[`RM][7:5] != 0 ? 32 : r[`RM][4:0])
+            `define RIGHT       (`IS_ROR ? { 1'b0, r[`RM][4:0] } : { r[`RM][7:5] != 0, r[`RM][4:0] })
 
             // BITWISE MUX
             //
@@ -166,12 +180,17 @@ always @(posedge sck) begin
             // C3 = B1 ? ~B : B
             // C4 = C0 & C1 | C2 & C3
             // 
-            // FUNC    EXPR                 B1  B0
+            // FUNC    EXPR                 m1  m0
             // and     A & B                0   0
             // or      A | B                0   1
             // xor     A ^ B                1   0
             // nand  ~(A & B) -> ~A | ~B    1   1
-            if (`IS_TST || `IS_AND || `IS_XOR || `IS_ORR || `IS_BIC) begin
+
+            // T1: AND RDN, RM
+            // T1: XOR RDN, RM
+            // T1: TST RDN, RM
+            // T1: ORR RDN, RM
+            if (`IS_AND || `IS_XOR || `IS_TST || `IS_ORR || `IS_BIC) begin
                 a  = `IS_BIC ? ~r[`RM] : r[`RM];
                 m0 = `IS_ORR;
                 m1 = `IS_XOR;
@@ -181,46 +200,46 @@ always @(posedge sck) begin
                 if (`IS_NOT_TST) begin
                     r[`RDN] = b;
                 end
+
+                tmp_cf = cf;    // cf 不变
+                tmp_vf = vf;    // vf 不变
+                modify_state = `IS_TST || `NOT_IN_ITB;
+            // T1: MUL RDN, RM
             end else if (`IS_MUL) begin
                 r[`RDN] *= r[`RM];
                 b = r[`RDN];
-                tmp_cf = cf; // cf 不变，所以赋值给 tmp_cf
+                tmp_cf = cf;    // cf 不变
+                tmp_vf = vf;    // vf 不变
+                modify_state = `NOT_IN_ITB;
+            // T1: LSL RDN, RM
             end else if (`IS_LSL) begin
                 { tmp_cf, r[`RDN] } = { cf, r[`RDN] } << `LEFT;
                 { b } = r[`RDN];
+                { tmp_vf } = vf;
+                { modify_state } = `NOT_IN_ITB;
             end else if (`IS_LSR || `IS_ASR || `IS_ROR) begin
                 { r[`RDN], a } = { 
-                    `IS_ASR && r[`RDN][`INT_MSB] ? `ALL_ONE : `ALL_ZERO, r[`RDN], `ALL_ZERO
+                    `IS_ASR && r[`RDN][`REG_MSB] ? `ALL_ONE : `ALL_ZERO, r[`RDN], `ALL_ZERO
                 } >> `RIGHT;
 
-                { tmp_cf } = a[`INT_MSB];
                 { r[`RDN] } |= `IS_ROR ? a : `ALL_ZERO;
                 { b } = r[`RDN];
+                { tmp_cf } = a[`REG_MSB];
+                { tmp_vf } = vf;
+                { modify_state } = `NOT_IN_ITB;
             // `IS_CMN || `IS_CMP || `IS_ADC || `IS_SBC || `IS_RSB || `IS_MVN
             end else begin
                 { a } = `IS_MVN ? 0 : r[`RDN];
-                { b, sub_cf } = `IS_SBC || `IS_CMP || `IS_MVN ? { ~a, !`IS_MVN } : { a, 1'b0 };
-                { tmp_vf } = r[`RDN][`INT_MSB] == b[`INT_MSB];
-                { tmp_cf, b } = a + b + sub_cf;
-                { tmp_vf } = tmp_vf ? tmp_vf != b[`INT_MSB] : 0;
-                { need_modify_vf } = !`IS_MVN;
+                { b } = `IS_SBC || `IS_CMP || `IS_MVN ? ~a  + `IS_NOT_MVN : a;
+                { msb_eq } = r[`RDN][`REG_MSB] == b[`REG_MSB];
+                { tmp_cf, b } = a + b;
+                { tmp_vf } = `IS_MVN ? vf : (msb_eq ? r[`RDN][`REG_MSB] != b[`REG_MSB] : 0);
+                { modify_state } = `NOT_IN_ITB || `IS_CMN || `IS_CMP;
 
-                if (!(`IS_CMN || `IS_CMP)) begin
+                if (`IS_NOT_CMN && `IS_NOT_CMP) begin
                     r[`RDN] = b;
                 end
             end
-
-            if (`IS_TST || `IS_CMN || `IS_CMP || !in_it_block) begin
-                nf = b[`INT_MSB];
-                zf = b == 0;
-                cf = tmp_cf;
-
-                if (need_modify_vf) begin
-                    vf = tmp_vf;
-                end
-            end
-
-            need_modify_vf = 0;
 
             `undef IS_AND
             `undef IS_XOR
@@ -239,6 +258,9 @@ always @(posedge sck) begin
             `undef IS_BIC
             `undef IS_MVN
             `undef IS_NOT_TST
+            `undef IS_NOT_CMP
+            `undef IS_NOT_CMN
+            `undef IS_NOT_MVN
             `undef RM
             `undef RDN
             `undef LEFT
@@ -249,4 +271,13 @@ always @(posedge sck) begin
             
         end
     endcase
+
+    if (modify_state) begin
+        modify_state = 0;
+        nf = b[`REG_MSB];
+        zf = b == 0;
+        cf = tmp_cf;
+        vf = tmp_vf;
+    end
+
 end endmodule
