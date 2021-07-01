b0be0f7ae731e3b99837594e5227894aebaa62a7
PowerfulCat <x4495@outlook.com>
2021-07-01 23:55:52 +0800

    新增访存指令系列 @ extern/arm/vcpu.v

--- extern/arm/vcpu.v
+++ extern/arm/vcpu.v
@@ -8,10 +8,11 @@
 `define ALL_ZERO            32'h0000_0000
 `define NOT_IN_ITB          (!in_it_block)
 `define NOT_LAST_IN_ITB     (!last_in_it_block)
-`define IN_ITB              (in_it_block)
 `define SYS_BITS            32
 `define SYS_BITS_PLUS1      6'd33
 `define SYS_BITS_SUB3       29
+`define SYS_BITS_SUB8       24
+`define SYS_BITS_SUB16      16
 `define SYS_BITS_SUB8       24
 
 // 修改一个寄存器的放在低组
@@ -20,6 +21,7 @@
 `define I_ADD               1
 `define I_MUL               2
 `define I_BITWISE           3
+`define I_MEM               4
 
 // 修改 2 个寄存器的放在高组
 `define B_HIG               10
@@ -58,6 +60,7 @@ reg [31:0] tmp_ds_shift  ;
 reg [31:0] tmp_ds_add    ;
 reg [31:0] tmp_ds_mul    ;
 reg [31:0] tmp_ds_bitwise;
+reg [31:0] tmp_ds_mem    ;
 reg [31:0] tmp_ds_bl     ;
 
 reg [31:0] tmp_es_bl;
@@ -67,6 +70,7 @@ reg tmp_cf_add    , tmp_vf_add    , msb_eq_add    , tmp_zf_add    , tmp_of_add  
 reg tmp_cf_mul    , tmp_vf_mul    , msb_eq_mul    , tmp_zf_mul    , tmp_of_mul    ;
 reg tmp_cf_bitwise, tmp_vf_bitwise, msb_eq_bitwise, tmp_zf_bitwise, tmp_of_bitwise;
 reg tmp_cf_bl     , tmp_vf_bl     , msb_eq_bl     , tmp_zf_bl     , tmp_of_bl     ;
+reg tmp_cf_mem    , tmp_vf_mem    , msb_eq_mem    , tmp_zf_mem    , tmp_of_mem    ;
 
 // 修改该标志位寄存器
 reg modify_state_req = 0;
@@ -85,6 +89,7 @@ assign d     [`I_SHIFT  ] = tmp_ds_shift  ;
 assign d     [`I_ADD    ] = tmp_ds_add    ;
 assign d     [`I_MUL    ] = tmp_ds_mul    ;
 assign d     [`I_BITWISE] = tmp_ds_bitwise;
+assign d     [`I_MEM    ] = tmp_ds_mem    ;
 assign d     [`I_BL     ] = tmp_ds_bl     ;
 assign e     [`I_BL     ] = tmp_es_bl     ;
 
@@ -205,6 +210,38 @@ always @(posedge (bl_req != bl_ack)) begin
     { bl_ack } = bl_req;
 end
 
+// 内存加载指令
+reg mem_req                 = 0;
+reg mem_ack                 = 0;
+reg mem_mode                = 0;
+reg mem_is_signed           = 0;
+reg [ 1:0] mem_scale        = 0;
+reg [31:0] mem_addr         = 0;
+
+`define MEM_MODE_READ       1'b0
+`define MEM_MODE_WRITE      1'b1
+`define MEM_SCALE_8BIT      2'b00
+`define MEM_SCALE_16BIT     2'b01
+`define MEM_SCALE_32BIT     2'b10
+
+always @(posedge (mem_req != mem_ack)) begin
+    { mem_addr } = n + m;
+    { tmp_ds_mem } = mem_addr; // TODO：内存访问模块
+
+    if (mem_is_signed && mem_mode == `MEM_MODE_READ) begin
+        casez(mem_scale)
+        `MEM_SCALE_8BIT : begin
+            tmp_ds_mem = { { `SYS_BITS_SUB8 {tmp_ds_mem[ 7]} }, tmp_ds_mem[ 7:0] };
+        end
+        `MEM_SCALE_16BIT: begin
+            tmp_ds_mem = { { `SYS_BITS_SUB16{tmp_ds_mem[15]} }, tmp_ds_mem[15:0] };
+        end
+        endcase
+    end
+
+    { mem_ack } = !mem_req;
+end
+
 always @(posedge sck) begin
     // 大部分指令只有一个目的寄存器
     // 一般只要写入 DS 对应的寄存器就好了
@@ -322,8 +359,8 @@ always @(posedge sck) begin
 
                 // 关于 SBC
                 // d = n - m - !cf
-                // d = n - ~m + (1 - !cf)
-                // d = n - ~m + cf
+                // d = n + ~m + (1 - !cf)
+                // d = n + ~m + cf
             end
         end
 
@@ -365,10 +402,30 @@ always @(posedge sck) begin
             end
         end
 
+        // load from literal pool
+        8'b0100_1???: begin
+            { i_serial } = `I_MEM;
+            { mem_mode } = `MEM_MODE_READ;
+            { mem_scale } = `MEM_SCALE_32BIT;
+            { mem_is_signed } = 0;
+            { reg_ds } = cmd[10:8];
+            // 确认一下 PC 是否需要对齐
+            { n } = r[`PC];
+            { m } = { cmd[7:0], 2'b0 };
+            { mem_req } = !mem_ack;
+        end
+
+        // load/store register offset
+        8'b0101_????: begin
+            
+        end
+
+        // load/store word/byte immediate offset
+
         default: begin
             
         end
-    endcase    
+    endcase
 end
 
 always @(negedge sck) begin
