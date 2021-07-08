ac5c3f6f750910243a4975e87222c67253f04dcc
PowerfulCat <x4495@outlook.com>
2021-07-09 00:03:42 +0800

    arm 平台边边角角调整，简化初始化逻辑，移除可以不必复位的寄存器 @ extern/arm/vcpu.v

--- extern/arm/vcpu.v
+++ extern/arm/vcpu.v
@@ -49,11 +49,11 @@ reg        zf;
 reg        nf;
 reg        vf;
 
-reg [ 7:0] multi_list;
-reg        multi_list_lr;
-reg        multi_list_adjust_base; // 基址 - 待保存寄存器数 * 4
-reg        multi_list_adjust_by_add;
-reg        multi_access;
+reg [ 7:0] multi_list = 0;
+reg        multi_list_lr = 0;
+reg        multi_list_adjust_base = 0; // 基址 - 待保存寄存器数 * 4
+reg        multi_list_adjust_by_add = 0;
+reg        multi_access = 0;
 reg        tmp_make_jmp;
 reg        tmp_error;
 
@@ -128,81 +128,64 @@ reg        reset_ack = 0;
 reg        reset = 0;
 
 always @(posedge rst) begin
-    // 不允许在复位为完成时再发起复位信号
-    if (reset_req == reset_ack) begin
-        reset_req = !reset_ack;
-    end
+    reset_req = !reset_ack;
 end
 
 // 移位操作 ========================================
 `define SHIFT_LEFT          1'b0
 `define SHIFT_RIGHT         1'b1
 
-reg [31:0]  shift_temp;
-reg [5:0]   shift_bits;
-reg         shift_direction;    // 0 : left / 1 : right
-reg         shift_with_sign;    // 带符号移位，只对右移有效
-reg         shift_with_loop;    // 循环移位，只对右移有效
-reg         shift_req;
-reg         shift_ack;
+reg [31:0]  shift_temp = 0;
+reg [5:0]   shift_bits = 0;
+reg         shift_direction = 0;    // 0 : left / 1 : right
+reg         shift_with_sign = 0;    // 带符号移位，只对右移有效
+reg         shift_with_loop = 0;    // 循环移位，只对右移有效
+reg         shift_req = 0;
+reg         shift_ack = 0;
 
-always @(posedge (shift_req != shift_ack) or posedge reset) begin
-    if (reset) begin
-        { shift_temp } = 0;
-        { shift_bits } = 0;
-        { shift_ack } = 0;
-    end else begin
-        { shift_bits } = m[7:0] > `SYS_BITS ? `SYS_BITS_PLUS1 : m[5:0];
+always @(posedge (shift_req != shift_ack)) begin
+    { shift_bits } = m[7:0] > `SYS_BITS ? `SYS_BITS_PLUS1 : m[5:0];
 
-        if (shift_direction == `SHIFT_LEFT) begin // 左移
-            { tmp_cf_shift, tmp_ds_shift } = { cf, n } << shift_bits;
-        end else begin // 右移
-            { tmp_ds_shift, shift_temp } = {
-                { `SYS_BITS_PLUS1{ shift_with_sign && n[`REG_MSB] } }, n, `ALL_ZERO
-            } >> (shift_with_loop ? { 1'b0, m[4:0] } : { shift_bits });
+    if (shift_direction == `SHIFT_LEFT) begin // 左移
+        { tmp_cf_shift, tmp_ds_shift } = { cf, n } << shift_bits;
+    end else begin // 右移
+        { tmp_ds_shift, shift_temp } = {
+            { `SYS_BITS_PLUS1{ shift_with_sign && n[`REG_MSB] } }, n, `ALL_ZERO
+        } >> (shift_with_loop ? { 1'b0, m[4:0] } : { shift_bits });
 
-            { tmp_ds_shift } = shift_temp[`REG_MSB];
+        { tmp_ds_shift } = shift_temp[`REG_MSB];
 
-            if (shift_with_loop) begin
-                { tmp_ds_shift } = tmp_ds_shift | shift_temp;
-            end
+        if (shift_with_loop) begin
+            { tmp_ds_shift } = tmp_ds_shift | shift_temp;
         end
-
-        // 不更改 vf
-        { tmp_vf_shift } = vf;
-        { shift_ack } = shift_req;
     end
+
+    // 不更改 vf
+    { tmp_vf_shift } = vf;
+    { shift_ack } = shift_req;
 end
 
 // 加法操作 ========================================
-reg add_with_cf;
-reg add_req;
-reg add_ack;
+reg add_with_cf = 0;
+reg add_req = 0;
+reg add_ack = 0;
 
-always @(posedge (add_req != add_ack) or posedge reset) begin
-    if (reset) begin
-        { add_ack } = 0;
-    end else begin
-        { msb_eq_add } = n[`REG_MSB] == m[`REG_MSB];
-        { tmp_cf_add, tmp_ds_add } = n + m + add_with_cf;
-        { tmp_vf_add } = msb_eq_add ? n[`REG_MSB] != tmp_ds_add[`REG_MSB] : 0;
-        { add_ack } = add_req;
-    end
+always @(posedge (add_req != add_ack)) begin
+    { msb_eq_add } = n[`REG_MSB] == m[`REG_MSB];
+    { tmp_cf_add, tmp_ds_add } = n + m + add_with_cf;
+    { tmp_vf_add } = msb_eq_add ? n[`REG_MSB] != tmp_ds_add[`REG_MSB] : 0;
+    { add_ack } = add_req;
 end
 
 // 乘法操作 ========================================
-reg mul_req;
-reg mul_ack;
+reg mul_req = 0;
+reg mul_ack = 0;
 
-always @(posedge (mul_req != mul_ack) or posedge reset) begin
-    if (reset) begin
-        { mul_ack } = 0;
-    end else begin
-        { tmp_ds_mul } = n * m;
-        { tmp_cf_mul } = cf;
-        { tmp_vf_mul } = vf;
-        { mul_ack } = mul_req;
-    end
+always @(posedge (mul_req != mul_ack)) begin
+    { tmp_ds_mul } = n * m;
+    { tmp_cf_mul } = cf;
+    { tmp_vf_mul } = vf;
+    { mul_ack } = mul_req;
 end
 
 // 按位操作 ========================================
@@ -219,39 +202,32 @@ end
 // or      A | B                0   1
 // xor     A ^ B                1   0
 // nand  ~(A & B) -> ~A | ~B    1   1
-reg bitwise_m0;
-reg bitwise_m1;
-reg bitwise_req;
-reg bitwise_ack;
+reg bitwise_m0 = 0;
+reg bitwise_m1 = 0;
+reg bitwise_req = 0;
+reg bitwise_ack = 0;
 
-always @(posedge (bitwise_req != bitwise_ack) or posedge reset) begin
-    if (reset) begin
-        { bitwise_ack } = 0;
-    end else begin
-        { tmp_ds_bitwise } =
-            ((bitwise_m1 ? ~n       : n) & (bitwise_m0 ? `ALL_ONE : m)) | 
-            ((bitwise_m0 ? `ALL_ONE : n) & (bitwise_m1 ? ~m       : m));
-        { tmp_vf_bitwise } = vf;
-        { bitwise_ack } = bitwise_req;
-    end
+always @(posedge (bitwise_req != bitwise_ack)) begin
+    { tmp_ds_bitwise } =
+        ((bitwise_m1 ? ~n       : n) & (bitwise_m0 ? `ALL_ONE : m)) | 
+        ((bitwise_m0 ? `ALL_ONE : n) & (bitwise_m1 ? ~m       : m));
+    { tmp_cf_bitwise } = cf;
+    { tmp_vf_bitwise } = vf;
+    { bitwise_ack } = bitwise_req;
 end
 
 // 转移指令
-reg bl_req;
-reg bl_ack;
-reg bl_with_link;
+reg bl_req = 0;
+reg bl_ack = 0;
+reg bl_with_link = 0;
 
-always @(posedge (bl_req != bl_ack) or posedge reset) begin
-    if (reset) begin
-        { bl_ack } = 0;
-    end else begin
-        if (bl_with_link) begin
-            { tmp_es_bl } = r[`PC] + 3/*2 + 1*/;
-        end
-
-        { tmp_ds_bl } = m;
-        { bl_ack } = bl_req;
+always @(posedge (bl_req != bl_ack)) begin
+    if (bl_with_link) begin
+        { tmp_es_bl } = r[`PC] + 3/*2 + 1*/;
     end
+
+    { tmp_ds_bl } = m;
+    { bl_ack } = bl_req;
 end
 
 // 内存加载指令
@@ -275,27 +251,22 @@ reg [31:0] mem_addr         = 0;
 `define MEM_SCALE_16BIT     2'b01
 `define MEM_SCALE_32BIT     2'b00
 
-always @(posedge (mem_req != mem_ack) or posedge reset) begin
-    if (reset) begin
-        { mem_ack } = 0;
-        { mem_addr } = 0;
-    end else begin
-        { mem_addr } = n + m;
-        { tmp_ds_mem } = mem_addr; // TODO：内存访问模块
+always @(posedge (mem_req != mem_ack)) begin
+    { mem_addr } = n + m;
+    { tmp_ds_mem } = mem_addr; // TODO：内存访问模块
 
-        if (mem_is_signed && mem_mode == `MEM_MODE_READ) begin
-            casez(mem_scale)
-            `MEM_SCALE_8BIT : begin
-                tmp_ds_mem = { { `SYS_BITS_SUB8 {tmp_ds_mem[ 7]} }, tmp_ds_mem[ 7:0] };
-            end
-            `MEM_SCALE_16BIT: begin
-                tmp_ds_mem = { { `SYS_BITS_SUB16{tmp_ds_mem[15]} }, tmp_ds_mem[15:0] };
-            end
-            endcase
+    if (mem_is_signed && mem_mode == `MEM_MODE_READ) begin
+        casez(mem_scale)
+        `MEM_SCALE_8BIT : begin
+            tmp_ds_mem = { { `SYS_BITS_SUB8 {tmp_ds_mem[ 7]} }, tmp_ds_mem[ 7:0] };
         end
-
-        { mem_ack } = mem_req;
+        `MEM_SCALE_16BIT: begin
+            tmp_ds_mem = { { `SYS_BITS_SUB16{tmp_ds_mem[15]} }, tmp_ds_mem[15:0] };
+        end
+        endcase
     end
+
+    { mem_ack } = mem_req;
 end
 
 always @(posedge sck) begin
@@ -312,23 +283,7 @@ always @(posedge sck) begin
         { last_in_it_block } = 0;
         { multi_access } = 0;
 
-        { shift_direction } = 0;
-        { shift_with_sign } = 0;
-        { shift_with_loop } = 0;
-        { shift_req } = 0;
-
-        { add_with_cf } = 0;
-
-        { mul_req } = 0;
-
-        { bitwise_m0 } = 0;
-        { bitwise_m1 } = 0;
-        { bitwise_req } = 0;
-
-        { bl_req } = 0;
-        { bl_with_link } = 0;
-
-        { mem_req } = 0;
+        { mem_req } = mem_ack;
         { mem_mode } = 0;
         { mem_is_signed } = 0;
         { mem_continue } = 0;
@@ -791,7 +746,7 @@ always @(posedge sck) begin
             4'b0111: begin mem_buffer_i[0] = 0; mem_buffer_i[1] = 1; mem_buffer_i[2] = 2; mem_end = 3; end
             4'b1000: begin mem_buffer_i[0] = 3; mem_end = 1; end
             4'b1001: begin mem_buffer_i[0] = 0; mem_buffer_i[1] = 3; mem_end = 2; end
-            4'b1010: begin mem_buffer_i[0] = 1; mem_buffer_i[1] = 3; mem_end = 2;  end
+            4'b1010: begin mem_buffer_i[0] = 1; mem_buffer_i[1] = 3; mem_end = 2; end
             4'b1011: begin mem_buffer_i[0] = 0; mem_buffer_i[1] = 1; mem_buffer_i[2] = 3; mem_end = 3; end
             4'b1100: begin mem_buffer_i[0] = 2; mem_buffer_i[1] = 3; mem_end = 2;  end
             4'b1101: begin mem_buffer_i[0] = 0; mem_buffer_i[1] = 2; mem_buffer_i[2] = 3; mem_end = 3; end
@@ -810,7 +765,7 @@ always @(posedge sck) begin
             4'b0111: begin mem_buffer_i[mem_end + 0] = 0; mem_buffer_i[mem_end + 1] = 1; mem_buffer_i[mem_end + 2] = 2; mem_end = mem_end + 3; end
             4'b1000: begin mem_buffer_i[mem_end + 0] = 3; mem_end = mem_end + 1; end
             4'b1001: begin mem_buffer_i[mem_end + 0] = 0; mem_buffer_i[mem_end + 1] = 3; mem_end = mem_end + 2; end
-            4'b1010: begin mem_buffer_i[mem_end + 0] = 1; mem_buffer_i[mem_end + 1] = 3; mem_end = mem_end + 2;  end
+            4'b1010: begin mem_buffer_i[mem_end + 0] = 1; mem_buffer_i[mem_end + 1] = 3; mem_end = mem_end + 2; end
             4'b1011: begin mem_buffer_i[mem_end + 0] = 0; mem_buffer_i[mem_end + 1] = 1; mem_buffer_i[mem_end + 2] = 3; mem_end = mem_end + 3; end
             4'b1100: begin mem_buffer_i[mem_end + 0] = 2; mem_buffer_i[mem_end + 1] = 3; mem_end = mem_end + 2;  end
             4'b1101: begin mem_buffer_i[mem_end + 0] = 0; mem_buffer_i[mem_end + 1] = 2; mem_buffer_i[mem_end + 2] = 3; mem_end = mem_end + 3; end
